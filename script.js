e071a4067b913215843de008e79a5dacaad62b61
Willyan Ferreira <willyan_carlos@yahoo.com.br>
2022-05-02 23:37:14 -0300

    Desafio: refazendo o projeto UNES
    Quando eu usava a codificação JS dentro de um arquivo JS, o body não exibia o conteúdo. Descobri o pq. Eu estava chamando o script no head aí lembrei que chamar o script no body faz com q a página seja primeiro carregada para depois chamar o scirpt e agora está funcionando.

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-// const table = document.createElement('h1');
-// table.innerHTML = "Willyan Carlos F.S.";
-// document.body.appendChild(table);
+        //Header do Site
+        const table = document.createElement('table');
+        const tr_header = document.createElement('tr');
+        const td_logo = document.createElement('td');
+        const td_menu = document.createElement('td');
 
+        document.body.appendChild(table);
+        table.appendChild(tr_header);
+
+        
+        const img_logo = document.createElement('img');
+        img_logo.src = 'img/logo.png';
+        td_logo.appendChild(img_logo);
+        td_menu.innerHTML = 'Só Jesus Salva.';
+
+        tr_header.appendChild(td_logo);
+        tr_header.appendChild(td_menu);
+
+        //Body do Site
+
+        //Footer do Site
