4e8bdb0714cc12be1f36d1ba283f94a895f6c5e3
Willyan Ferreira <willyan_carlos@yahoo.com.br>
2022-05-03 22:44:08 -0300

    Desafio: refazendo o projeto UNES
    - Começando a desenvolver a estrutura do body.
    - Aprendi a usar createAttribute para inserir um atributo em uma tag HTML usando JS.

--- script.js
+++ script.js
@@ -17,5 +17,15 @@
         tr_header.appendChild(td_menu);
 
         //Body do Site
+        const tr_capa = document.createElement('tr');
+        const td_capa = document.createElement('td');
+        td_capa.setAttribute('colspan', 2);
+        
+        const img_capa = document.createElement('img');
+        img_capa.src = 'img/capa.png';
+
+        table.appendChild(tr_capa);
+        tr_capa.appendChild(td_capa);
+        td_capa.appendChild(img_capa);
 
         //Footer do Site
