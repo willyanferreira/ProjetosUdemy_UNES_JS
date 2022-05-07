846ffcc6a2d6a28a3e1c445010e91f71008c6e6b
Willyan Ferreira <willyan_carlos@yahoo.com.br>
2022-05-06 22:31:34 -0300

    Desafio: refazendo o projeto UNES
    - add conteúdo no body
    - add footer

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
         //Header do Site
         const table = document.createElement('table');
+        table.setAttribute('align', 'center');
+        table.setAttribute('width', '900');
+
         const tr_header = document.createElement('tr');
         const td_logo = document.createElement('td');
         const td_menu = document.createElement('td');
@@ -25,10 +28,15 @@
 
         const link_home = document.createElement('a');
         link_home.innerHTML = "HOME";
+        link_home.setAttribute('href', '#');
+
         const link_quemSomos = document.createElement('a');
         link_quemSomos.innerHTML = "QUEM SOMOS";
+        link_quemSomos.setAttribute('href', 'quemSomos.html');
+
         const link_contato = document.createElement('a');
         link_contato.innerHTML = "CONTATO";
+        link_contato.setAttribute('href', 'contato.html');
 
         td_menu.appendChild(ul_menu);
 
@@ -52,4 +60,32 @@
         tr_capa.appendChild(td_capa);
         td_capa.appendChild(img_capa);
 
+        const tr_content = document.createElement('tr');
+
+        const td_content = document.createElement('td');
+        td_content.setAttribute('colspan', 2);
+
+        const h2_content = document.createElement('h2');
+        h2_content.innerHTML = 'Sobre a UNES';
+
+        const paragrafo = document.createElement('p');
+        paragrafo.innerHTML = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit fuga repudiandae atque maiores praesentium incidunt. Ducimus, explicabo, obcaecati natus, recusandae in rem a nemo dolorum at pariatur blanditiis magnam eum.';
+
+        table.appendChild(tr_content);
+        tr_content.appendChild(td_content);
+        td_content.appendChild(h2_content);
+        td_content.appendChild(paragrafo);
+
         //Footer do Site
+        const tr_footer = document.createElement('tr');
+
+        const td_footer = document.createElement('td');
+        td_footer.setAttribute('colspan',2);
+        td_footer.setAttribute('align', 'center');
+
+        const h4_footer = document.createElement('h4');
+        h4_footer.innerHTML = 'Todos os direitos reservados';
+
+        table.appendChild(tr_footer);
+        tr_footer.appendChild(td_footer);
+        td_footer.appendChild(h4_footer);
